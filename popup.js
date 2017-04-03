a29a6e7d9e814fe854d6964a5b3c11bdad0478ce
MartinMouritzen <mmo@trustpilot.com>
2017-04-03 14:00:13 +0200

    Made sure we use tabs everywhere (spaces at work, tabs at home makes a poor commit).

--- popup.js
+++ popup.js
@@ -1,28 +1,28 @@
 function syntaxHighlight(json) {
-    if (typeof json != 'string') {
-         json = JSON.stringify(json, undefined, 2);
-    }
-    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+	if (typeof json != 'string') {
+		 json = JSON.stringify(json, undefined, 2);
+	}
+	json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
 
-    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-        var cls = 'number';
-        if (/^"/.test(match)) {
-            if (/:$/.test(match)) {
-                cls = 'key';
-                return '<span class="' + cls + '">' + match + '</span>';
-            } else {
-                cls = 'string';
-                return '<span class="' + cls + '">' + match + '</span>';
-            }
-        } else if (/true|false/.test(match)) {
-            cls = 'boolean';
-            return '<span class="' + cls + '">' + match + '</span>';
-        } else if (/null/.test(match)) {
-            cls = 'null';
-            return '<div class="' + cls + '">' + match + '</div>';
-        }
-        return '<span class="' + cls + '">' + match + '</span>';
-    });
+	return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+		var cls = 'number';
+		if (/^"/.test(match)) {
+			if (/:$/.test(match)) {
+				cls = 'key';
+				return '<span class="' + cls + '">' + match + '</span>';
+			} else {
+				cls = 'string';
+				return '<span class="' + cls + '">' + match + '</span>';
+			}
+		} else if (/true|false/.test(match)) {
+			cls = 'boolean';
+			return '<span class="' + cls + '">' + match + '</span>';
+		} else if (/null/.test(match)) {
+			cls = 'null';
+			return '<div class="' + cls + '">' + match + '</div>';
+		}
+		return '<span class="' + cls + '">' + match + '</span>';
+	});
 }
 function showEvent(number) {
 	document.getElementById('eventContent_' + number).style.display = 'block';
@@ -57,7 +57,7 @@ function printVariable(jsonObject,level) {
 }
 
 var port = chrome.extension.connect({
-    name: "trackPopup"
+	name: "trackPopup"
 });
 port.postMessage({
 	type: "update"
@@ -68,33 +68,33 @@ port.onMessage.addListener(function(msg) {
 		// console.log(jsonObject);
 		
 		var prettyEventsString = '';
-    	for (var i=0;i<msg.events.length;i++) {
-    		var event = msg.events[i];
-    		
+		for (var i=0;i<msg.events.length;i++) {
+			var event = msg.events[i];
+			
 			var jsonObject = JSON.parse(event.raw)
-    		var eventString = '';
-    		eventString += '<div class="eventTracked">';
-    			eventString += '<div class="eventInfo" number="' + i + '"><span class="eventName">' + event.eventName + '</span> - ' + event.trackedTime + '<br />' + event.hostName + '</div>';
-	    		eventString += '<div class="eventContent" id="eventContent_' + i + '">';
-	    			eventString += printVariable(jsonObject,0);
-	    		eventString += '</div>';
-    		eventString += '</div>';
-    		
-    		prettyEventsString += eventString;
-    	}
-    	document.getElementById('trackMessages').innerHTML = prettyEventsString;
+			var eventString = '';
+			eventString += '<div class="eventTracked">';
+				eventString += '<div class="eventInfo" number="' + i + '"><span class="eventName">' + event.eventName + '</span> - ' + event.trackedTime + '<br />' + event.hostName + '</div>';
+				eventString += '<div class="eventContent" id="eventContent_' + i + '">';
+					eventString += printVariable(jsonObject,0);
+				eventString += '</div>';
+			eventString += '</div>';
+			
+			prettyEventsString += eventString;
+		}
+		document.getElementById('trackMessages').innerHTML = prettyEventsString;
 
-    	var eventElements = document.getElementsByClassName("eventInfo");
-    	for (var i=0;i<eventElements.length;i++) {
-    		eventElements[i].onclick = function() {
-    			var number = this.getAttribute('number');
-    			if (document.getElementById('eventContent_' + number).style.display == 'block') {
-    				document.getElementById('eventContent_' + number).style.display = 'none';
-    			}
-    			else {
-    				document.getElementById('eventContent_' + number).style.display = 'block';
-    			}
-    		}
-    	}
-    }
+		var eventElements = document.getElementsByClassName("eventInfo");
+		for (var i=0;i<eventElements.length;i++) {
+			eventElements[i].onclick = function() {
+				var number = this.getAttribute('number');
+				if (document.getElementById('eventContent_' + number).style.display == 'block') {
+					document.getElementById('eventContent_' + number).style.display = 'none';
+				}
+				else {
+					document.getElementById('eventContent_' + number).style.display = 'block';
+				}
+			}
+		}
+	}
 });
