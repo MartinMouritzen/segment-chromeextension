5a462ceedab44c3dd371e246c9c25f0668071a3c
Bruno Vezoli <c-bruno.vezoli@masterclass.com>
2021-04-17 00:25:54 -0300

    Allow filtering events by name

--- popup.js
+++ popup.js
@@ -11,7 +11,7 @@ function printVariable(jsonObject,level) {
 			returnString += '<span class="key">';
 			returnString += key;
 			returnString += '</span>';
-			
+
 			if (typeof jsonObject[key] == 'object') {
 				returnString += ' {' + printVariable(jsonObject[key],level + 1) + '}';
 			}
@@ -20,7 +20,7 @@ function printVariable(jsonObject,level) {
 				if (isNaN(jsonObject[key])) {
 					type = 'string';
 				}
-				
+
 				returnString += ': ';
 				returnString += '<span class="' + type + '">';
 				returnString += jsonObject[key];
@@ -34,7 +34,7 @@ function printVariable(jsonObject,level) {
 function queryForUpdate() {
 	chrome.tabs.query({ active: true, currentWindow: true },(tabs) => {
 		var currentTab = tabs[0];
-		
+
 		port.postMessage({
 			type: "update",
 			tabId: currentTab.id
@@ -45,7 +45,7 @@ function queryForUpdate() {
 function clearTabLog() {
 	chrome.tabs.query({ active: true, currentWindow: true },(tabs) => {
 		var currentTab = tabs[0];
-		
+
 		port.postMessage({
 			type: "clear",
 			tabId: currentTab.id
@@ -62,23 +62,23 @@ queryForUpdate();
 port.onMessage.addListener((msg) => {
 	if (msg.type == "update") {
 		// console.log(jsonObject);
-		
+
 		var prettyEventsString = '';
-		
+
 		if (msg.events.length > 0) {
 			for (var i=0;i<msg.events.length;i++) {
 				var event = msg.events[i];
-				
+
 				var jsonObject = JSON.parse(event.raw)
 				var eventString = '';
-				
+
 				eventString += '<div class="eventTracked eventType_' + event.type + '">';
 					eventString += '<div class="eventInfo" number="' + i + '"><span class="eventName">' + event.eventName + '</span> - ' + event.trackedTime + '<br />' + event.hostName + '</div>';
 					eventString += '<div class="eventContent" id="eventContent_' + i + '">';
 						eventString += printVariable(jsonObject,0);
 					eventString += '</div>';
 				eventString += '</div>';
-				
+
 				prettyEventsString += eventString;
 			}
 		}
@@ -102,12 +102,25 @@ port.onMessage.addListener((msg) => {
 	}
 });
 
+function filterEvents(keyPressedEvent) {
+	var filter = new RegExp(keyPressedEvent.target.value, 'gi');
+	var eventElements = document.getElementById('trackMessages').getElementsByClassName('eventTracked');
+	for(eventElement of eventElements) {
+		var eventName = eventElement.getElementsByClassName('eventName')[0].textContent;
+		if (eventName.match(filter)) {
+			eventElement.classList.remove('hidden');
+		} else {
+			eventElement.classList.add('hidden');
+		}
+	}
+}
+
 function toggleConfiguration() {
 	var configurationDiv = document.getElementById('configurationDiv');
-	configurationDiv.hidden = !configurationDiv.hidden; 
+	configurationDiv.hidden = !configurationDiv.hidden;
 
 	var contentDiv = document.getElementById('contentDiv');
-	contentDiv.hidden = !contentDiv.hidden; 
+	contentDiv.hidden = !contentDiv.hidden;
 }
 
 function updateApiDomain(apiDomain) {
@@ -117,7 +130,7 @@ function updateApiDomain(apiDomain) {
 
 function handleApiDomainUpdates() {
 	var apiDomainInput = document.getElementById('apiDomain');
-	
+
 	chrome.storage.local.get(['segment_api_domain'], function(result) {
 		apiDomainInput.value = result.segment_api_domain || apiDomainDefault;
 		apiDomainInput.onchange = () => updateApiDomain(apiDomainInput.value);
@@ -127,6 +140,10 @@ function handleApiDomainUpdates() {
 document.addEventListener('DOMContentLoaded', function() {
 	var clearButton = document.getElementById('clearButton');
 	clearButton.onclick = clearTabLog;
+
+	var filterInput = document.getElementById('filterInput');
+	filterInput.onkeyup = filterEvents;
+	filterInput.focus();
 
 	var configureButton = document.getElementById('configureButton');
 	configureButton.onclick = toggleConfiguration;
